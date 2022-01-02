e3c2ffdcc0a35c3e6410039625443f60af70896a
Paulo Victor <paulovictorsantos0@gmail.com>
2022-01-02 12:56:00 -0300

    Configuraçao para compilaçao

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 const { app, BrowserWindow } = require('electron')
 
-
 const createWindow = async () => {
 
     let mainWindow = new BrowserWindow({
         minWidth: 950,
         width: 950,
         height: 600,
-        webPreferences: {
-            nodeIntegration: true
-        }
+        autoHideMenuBar: true,
+        icon: __dirname + "/logo.png"
     })
 
     mainWindow.loadURL(`file://${__dirname}/src/index.html`)
